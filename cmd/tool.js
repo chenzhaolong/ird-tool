d67a4bad305ccec1863c1b8653ddbff7ca90d91b
dragonchen <dragonchen@webank.com>
2020-06-03 11:48:26 +0800

    feat: 搭建tool环境

---
+++ cmd/tool.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+// @ts-ignore
+const program = require('commander');
+const path = require('path');
+const fs = require('fs');
+
+const packagePath = path.resolve(__dirname, '../package.json');
+let msg = fs.readFileSync(packagePath, { encoding: 'utf-8' });
+msg = JSON.parse(msg);
+
+program
+  .version(msg.version, '-v, --version')
+  .usage('<command> [option]')
+  .description('Desc: create and handle project by ird')
+  .parse(process.argv);
